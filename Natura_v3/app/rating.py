706720cd62defb4fe29cd6eb2e983e34920192f7
amandatell <51403678+amandatell@users.noreply.github.com>
2020-04-21 20:03:51 +0200

    betyg sparas i db för inloggad user
    man kan bara betygsätta en plats

--- Natura_v3/app/rating.py
+++ Natura_v3/app/rating.py
@@ -3,14 +3,18 @@ from app import app, db
 from app.models import User, places, ratings
 from flask_login import current_user, login_user
 from datetime import datetime
+from sqlalchemy import exc
 
 def save_user_rating(user_rating, name):
-    place_id = db.session.query(places.id).filter(places.name==name)
-    #STAVFEL I TABELL SKA VARA PLACEID
-    save_rating_to_db = ratings(userid=current_user.id, placeid=place_id, ratings=user_rating, datetime=datetime.utcnow())
-    db.session.add(save_rating_to_db)
-    db.session.commit()
-    return flash('Du har gett betyg.')
+    try:
+        place_id = db.session.query(places.id).filter(places.name==name)
+        save_rating_to_db = ratings(userid=current_user.id, placeid=place_id, ratings=user_rating, datetime=datetime.utcnow())
+        db.session.add(save_rating_to_db)
+        db.session.commit()
+        return flash('Du har gett betyg.')
+    except exc.IntegrityError:
+        db.session.rollback()
+        return flash('Du har redan gett betyg för denna plats!')
 
 def average_rating():
     pass

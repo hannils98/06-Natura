4b8e42f77032b60a746fc0002f8ce19917eb08d2
ak2296 <62262817+ak2296@users.noreply.github.com>
2020-04-22 21:13:32 +0200

    post fix

--- Natura_v3/app/models.py
+++ Natura_v3/app/models.py
@@ -81,7 +81,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)

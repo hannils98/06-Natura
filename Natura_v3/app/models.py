309d62b0fd2393ee222e4352f5559c458cf16afa
ak2296 <62262817+ak2296@users.noreply.github.com>
2020-04-22 22:44:00 +0200

    post fix

--- Natura_v3/app/models.py
+++ Natura_v3/app/models.py
@@ -22,7 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author',cascade="all, delete", passive_deletes=True, lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -81,7 +81,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"),)
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)

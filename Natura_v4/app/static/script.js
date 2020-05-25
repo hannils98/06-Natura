e627ac61861203eafcd1b4f39ee19122dbccc894
ak2296 <62262817+ak2296@users.noreply.github.com>
2020-05-25 14:47:34 +0200

    js

--- Natura_v4/app/static/script.js
+++ Natura_v4/app/static/script.js
@@ -1,35 +1,29 @@
-
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     $(".dropdown-toggle").dropdown();
-  });
-
-
+});
 
 $(document).ready(function () {
-        var url = window.location;
+    var url = window.location;
     // Will only work if string in href matches with location
-        $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
 
     // Will also work for relative and absolute hrefs
-        $('ul.nav a').filter(function () {
-            return this.href == url;
-        }).parent().addClass('active').parent().parent().addClass('active');
-    });
+    $('ul.nav a').filter(function () {
+        return this.href == url;
+    }).parent().addClass('active').parent().parent().addClass('active');
+});
 
 //image grid//
-    function getRandomSize(min, max) {
-        return Math.round(Math.random() * (max - min) + min);
-      }
-      
-      var allImages = "";
-      
-      for (var i = 0; i < 25; i++) {
-        var width = getRandomSize(500, 800);
-        var height =  getRandomSize(500, 800);
-        allImages += '<img src="/app/static/uploads/{{image}}" ';
-      }
-      
-      $('#photos').append(allImages);
+function getRandomSize(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
 
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+    var width = getRandomSize(500, 800);
+    var height = getRandomSize(500, 800);
+    allImages += '<img src="/app/static/uploads/{{image}}" ';
+}
+
+$('#photos').append(allImages);
--- Natura_v4/app/static/vanilla.js
+++ Natura_v4/app/static/vanilla.js
@@ -3,7 +3,7 @@ function removeMyImage() {
     for (let i = 0; i < remove.length; i++) {
         remove[i].addEventListener("click", function () {
             remove_image = confirm('Är du säker på att du vill ta bort bilden?')
-            if(remove_image) {
+            if (remove_image) {
                 imageID = (document.getElementsByClassName('images')[0].id);
                 window.location.href = '?imageid=' + imageID + "&remove=True";
             }
@@ -11,19 +11,19 @@ function removeMyImage() {
     }
 };
 
-function savedPlace(){
+function savedPlace() {
     const saved = document.getElementById('saved').value;
-    if(saved==='True'){
+    if (saved === 'True') {
         const heart = document.getElementById('heart');
         heart.classList.remove('far');
         heart.classList.add('fas')
 
-    }else{
+    } else {
         savePlace();
     }
 }
 
-function savePlace(){
+function savePlace() {
     const heart = document.getElementById('heart');
     heart.addEventListener("mouseover", function () {
         heart.classList.remove('far');
@@ -38,14 +38,13 @@ function savePlace(){
         heart.classList.remove('fas');
         heart.classList.add('far');
     });
-
-
 }
+
 
 function removeSavedPlace() {
     heart = document.getElementsByClassName('heart_btn');
     for (let i = 0; i < heart.length; i++) {
-        heart[i].addEventListener("mouseover", function() {
+        heart[i].addEventListener("mouseover", function () {
             heart[i].classList.remove('fas');
             heart[i].classList.add('far');
         });
